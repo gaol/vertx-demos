4eb784d38c75bf14601ada26edbb489b859ab04e
Lin Gao <aoingl@gmail.com>
2019-05-28 17:28:16 +0800

    orgnize the codes for simplicity

--- src/main/java/io/vertx/examples/MyVerticle.java
+++ src/main/java/io/vertx/examples/MyVerticle.java
@@ -7,6 +7,7 @@ import io.vertx.core.logging.LoggerFactory;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.client.HttpRequest;
 import io.vertx.reactivex.ext.web.client.HttpResponse;
 import io.vertx.reactivex.ext.web.client.WebClient;
 
@@ -25,22 +26,33 @@ public class MyVerticle extends AbstractVerticle {
         }
         final String theCity = city;
         WebClient webClient = WebClient.create(vertx);
-        webClient
-          .get(443, String.format("%s.lianjia.com", theCity), "/")
-          .ssl(true).rxSend()
+          cityHouseRegionIdRequest(webClient, theCity).rxSend()
           .map(MyVerticle::cityRegionId)
-          .flatMap(regionId -> webClient.get(443, String.format("%s.lianjia.com", theCity),
-                String.format("/fangjia/priceTrend/?region=city&region_id=%s", regionId)).ssl(true).rxSend())
+          .flatMap(regionId -> cityHousePriceRequest(webClient, theCity, regionId).rxSend())
           .map(MyVerticle::cityHousePrice)
           .map(JsonObject::toBuffer)
-          // .doOnError(err -> logger.error("Failed to get the price", err))
-          .subscribe(rc.response().getDelegate()::end, rc::fail);
+          .subscribe(s -> {
+            logger.info(String.format("House price of %s is: %s", theCity, s.toString()));
+            rc.response().getDelegate().end(s);
+          }, e -> {
+            logger.error(String.format("Failed to get house price of city: %s", theCity), e);
+            rc.response().setStatusCode(400).end(e.getMessage());
+          });
       });
 
     vertx.createHttpServer()
         .requestHandler(router)
         .listen(8080);
 
+  }
+
+  private static HttpRequest<Buffer> cityHouseRegionIdRequest(WebClient webClient, String city) {
+    return webClient.get(443, String.format("%s.lianjia.com", city), "/").ssl(true);
+  }
+
+  private static HttpRequest<Buffer> cityHousePriceRequest(WebClient webClient, String city, String regionId) {
+    return webClient.get(443, String.format("%s.lianjia.com", city),
+        String.format("/fangjia/priceTrend/?region=city&region_id=%s", regionId)).ssl(true);
   }
 
   private static JsonObject cityHousePrice(HttpResponse<Buffer> resp) {
