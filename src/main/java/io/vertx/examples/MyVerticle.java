e5ca900cd4c019539fe8cb7ac4b92fce690f978e
Lin Gao <aoingl@gmail.com>
2022-11-22 13:10:35 +0800

    Update to http server on different ports for the easy demo

--- src/main/java/io/vertx/examples/MyVerticle.java
+++ src/main/java/io/vertx/examples/MyVerticle.java
@@ -2,36 +2,27 @@ package io.vertx.examples;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.impl.logging.Logger;
-import io.vertx.core.impl.logging.LoggerFactory;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
 
 public class MyVerticle extends AbstractVerticle {
 
-    private final static Logger logger = LoggerFactory.getLogger(MyVerticle.class);
-
     @Override
     public void start(Promise<Void> startPromise) {
-        Router router = Router.router(vertx);
-        router.get("/java").handler(this::handleRequest);
-        vertx.createHttpServer().requestHandler(router).listen(8080)
+        vertx.createHttpServer().requestHandler(r -> r.response()
+                        .putHeader("Content-Type", "text/html")
+                        .end("<html><body><h1>Hello from vert.x in Java!</h1></body></html>"))
+                .listen(8000)
                 .onSuccess(s -> context.putLocal("httpServerPort", s.actualPort()))
                 .flatMap(s -> vertx.deployVerticle("server.groovy"))
+                .flatMap(d -> vertx.deployVerticle("io.vertx.examples.Server"))
                 .onComplete(r -> {
                     if (r.succeeded()) {
-                        logger.info(String.format("Starts HttpServer from Java at: %d", (Integer)context.getLocal("httpServerPort")));
+                        System.out.printf("Starts HttpServer from Java at: %d%n", (Integer) context.getLocal("httpServerPort"));
                         startPromise.complete();
                     } else {
                         r.cause().printStackTrace();
                         startPromise.fail(r.cause());
                     }
                 });
-    }
-
-    private void handleRequest(RoutingContext rc) {
-        rc.response().putHeader("Content-Type", "text/html")
-                .end("<html><body><h1>Hello from vert.x in Java!</h1></body></html>");
     }
 
 }
--- src/main/resources/server.groovy
+++ src/main/resources/server.groovy
@@ -1,14 +1,11 @@
-import io.vertx.ext.web.Router
-
-Router router = Router.router(vertx)
-router.get("/groovy").handler(ctx -> {
-    ctx.response().putHeader("Content-Type", "text/html")
-            .end("<html><body><h1>Hello from vert.x in Groovy!</h1></body></html>")
-})
-vertx.createHttpServer().requestHandler(router).listen(8080).onComplete(r -> {
-    if (r.succeeded()) {
-        println "Starts HttpServer from Groovy at: " + r.result().actualPort()
-    } else {
-        r.cause().printStackTrace()
-    }
-});
+vertx.createHttpServer().requestHandler(r -> r.response()
+        .putHeader("Content-Type", "text/html")
+        .end("<html><body><h1>Hello from vert.x in Groovy!</h1></body></html>"))
+        .listen(8001)
+        .onComplete(r -> {
+            if (r.succeeded()) {
+                println "Starts HttpServer from Groovy at: " + r.result().actualPort()
+            } else {
+                r.cause().printStackTrace()
+            }
+        });
