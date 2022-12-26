ff9797712638043331380a9d6302f72234a5bfba
Lin Gao <aoingl@gmail.com>
2022-12-26 14:32:56 +0800

    -Djava.net.preferIP4Stack=true when start verticle in clustered mode

--- start-verticle.sh
+++ start-verticle.sh
@@ -1,1 +1,1 @@
-java  -jar verticle/target/vertx-demos-kafka-verticle-*-fat.jar -cluster --cluster-host=127.0.0.1
+java -Djava.net.preferIP4Stack=true -jar verticle/target/vertx-demos-kafka-verticle-*-fat.jar -cluster --cluster-host=127.0.0.1
